d6ccbc89436c43f2932e7b216858f20d0035e33e
FloweyPY <anbpylearn@gmail.com>
2023-03-11 12:54:21 +0300

    Changes

---
+++ 12.py
@@ -0,0 +1,3 @@
+Number = input('Введите любое число')
+length = len(Number[1::2])
+print(length, Number[1::2])
--- 2pr.py
+++ 2pr.py
@@ -1,5 +1,5 @@
 import time
 while True:
     print('Попейте воды')
-    time.sleep(30)
+    time.sleep(10)
     break
